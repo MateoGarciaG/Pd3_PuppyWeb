53ad57f7fa9c241c71aabf2bd93f195ceb619ee8
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2022-02-27 12:28:47 +0100

    feat: :sparkles: Add Bootstrap to the project

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// JS
+import 'bootstrap';
 
 createApp(App).use(store).use(router).mount("#app");
